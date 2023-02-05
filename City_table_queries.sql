6ca44f8c0401c1bb89a8e3a1a9fe64ae8286d5d6
khannarashi <51528472+khannarashi@users.noreply.github.com>
2023-02-05 23:21:02 +0530

    Update City_table_queries.sql

--- City_table_queries.sql
+++ City_table_queries.sql
@@ -1,12 +1,12 @@
 CITY
  ________________________
-|Feild		|	Type	 |
+|Feild		    |	Type	      |
 |------------------------|
-|ID		    |NUMBER   	 |
-|NAME  		|VARCHAR2(17)|
+|ID		       |NUMBER   	  |
+|NAME  		   |VARCHAR2(17)|
 |COUNTRYCODE|VARCHAR2(3) |
 |DISTRICT  	|VARCHAR2(20)|
-|POPULATION	|NUMBER		 |
+|POPULATION	|NUMBER		    |
  ------------------------
 
 # Finding all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
